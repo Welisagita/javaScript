4d8ab63d0efff3ba0b4252e34362dfdeb13b3fbf
Weli Sagita <91396511+Welisagita@users.noreply.github.com>
2022-11-29 16:32:46 +0700

    Update and rename javascript_comments.js to 04. javascript_comments.js

--- Js Introduction/04. javascript_comments.js
+++ Js Introduction/04. javascript_comments.js
@@ -3,7 +3,7 @@
 // program to take the user's name
 let name = prompt("Enter your name: ");
 console.log("Name: " + name);
-```
+
 
 //Output
 //Enter your name: James
@@ -12,13 +12,14 @@ console.log("Name: " + name);
 //Another comment example
 // program to take the user's name
 
-let name1 = prompt("Enter your name: ");  // take input
-console.log("Name: " + name1); // print the name variable
+let nama = prompt("Enter your name: ");  // take input
+console.log("Name: " + nama); // print the name variable
+
 
 //Output
-Enter your name: James
-Name: James
-```
+//Enter your name: James
+//Name: James
+
 //Prevent Executing Code Using Comments
 
 // program to take user's name and age
@@ -37,9 +38,9 @@ console.log("name: " + name2);
 You can get the name from the user
 and display the result*/ 
 
-let name = prompt("Enter your name:");
-console.log("Name " + name);
-```
+let name3 = prompt("Enter your name:");
+console.log("Name " + name3);
+
 //Output
 //Enter your name: Sarah
 //Name Sarah
