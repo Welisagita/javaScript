6cace1096d24646b0c80ea9bf1e43d975203c816
Weli Sagita <91396511+Welisagita@users.noreply.github.com>
2022-11-29 16:27:31 +0700

    Create javascript_comments.js

---
+++ Js Introduction/javascript_comments.js
@@ -0,0 +1,56 @@
+//JavaScript Comments
+// Write Comment
+// program to take the user's name
+let name = prompt("Enter your name: ");
+console.log("Name: " + name);
+```
+
+//Output
+//Enter your name: James
+//Name: James
+
+//Another comment example
+// program to take the user's name
+
+let name1 = prompt("Enter your name: ");  // take input
+console.log("Name: " + name1); // print the name variable
+
+//Output
+Enter your name: James
+Name: James
+```
+//Prevent Executing Code Using Comments
+
+// program to take user's name and age
+let name2 = prompt("Enter a name: ");
+// let age = prompt("Enter age: ");
+console.log("name: " + name2);
+// console.log("age:", age);
+
+//Output
+//Enter name: Sarah
+//name: Sarah
+
+
+//JavaScript Multiline comments
+/*This program is used to take name of the user
+You can get the name from the user
+and display the result*/ 
+
+let name = prompt("Enter your name:");
+console.log("Name " + name);
+```
+//Output
+//Enter your name: Sarah
+//Name Sarah
+
+
+//Programmiz Quiz
+//Which of the following operators is used as a comment in JavaScript?
+
+//1. //
+//2. <!-- →
+//3. #
+//4. **
+
+//Answer: 1
