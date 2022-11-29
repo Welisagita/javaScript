abf4a2d4605a67785f8455648f2100dfaf18698a
Weli Sagita <91396511+Welisagita@users.noreply.github.com>
2022-11-29 09:05:31 +0700

    Create js_console_log.js

---
+++ Js Introduction/js_console_log.js
@@ -0,0 +1,43 @@
+//<h1>JavaScript console.log()<h1>
+//In this tutorial, you will learn about JavaScript console with the help of examples.
+
+//<h2>JavaScript console.log()</h2>
+//All modern browsers have a web console for debugging. The console.log() method is used to write messages to these consoles. For example,
+
+let sum = 44;
+console.log(sum);   // 44
+
+//When you run the above code, 44 is printed on the console.
+
+//To learn more about using a console, visit: JavaScript Getting Started.
+
+//<h2>console.log() Syntax</h2>
+//Its syntax is:
+
+console.log(message);
+//Here, the message refers to either a variable or a value.
+
+//Note: We will be using the console.log() method to display the output in our upcoming lessons.
+
+//<h3>Example 1: Print a Sentence</h3>
+// program to print a sentence
+
+// passing string
+console.log("I love JS");
+
+//Output: I love JS
+
+//<h3>Example 2: Print Values Stored in Variables</h3>
+// program to print variables values
+
+// storing values
+const greet = 'Hello';
+const name = 'Jack';
+
+console.log(greet + ' ' + name);
+
+//Output: Hello Jack
+
+//As you can see from these examples, console.log() makes it easier to see the value inside a variable. That's why it's commonly used for testing/debugging code.
+
+//The console object also has various methods other than console.log(). To learn more, visit JavaScript console.
