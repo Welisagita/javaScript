12e7225d173841eb66ce5092eb7d5db65191bc52
Weli Sagita <91396511+Welisagita@users.noreply.github.com>
2022-11-29 16:50:51 +0700

    Update 06. javascript_comments.js

--- Js Introduction/06. javascript_comments.js
+++ Js Introduction/06. javascript_comments.js
@@ -1,6 +1,7 @@
 //JavaScript Comments
 // Write Comment
 // program to take the user's name
+
 let name = prompt("Enter your name: ");
 console.log("Name: " + name);
 
@@ -8,6 +9,16 @@ console.log("Name: " + name);
 //Output
 //Enter your name: James
 //Name: James
+
+
+//If JavaScript is showing reference error “Prompt is not defined” you should do this: 
+//Solution 1: The most effective solution is we have to install “prompt-sync“. 
+//make sure you have also installed an updated version of npm and node, then write the below code in the terminal: npm install prompt-sync
+//Example: This example will demonstrate the use of prompt on the server side by using the “prompt-sync” package:
+//const prompt=require("prompt-sync")({sigint:true}); >> use this before the prompt
+//let name = prompt("What's your name");
+//console.log("hello"+name+"!");
+
 
 //Another comment example
 // program to take the user's name
