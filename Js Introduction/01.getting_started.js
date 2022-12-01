f0d8717f75b43fc6952661b70b7d957887f596fb
Weli Sagita <91396511+Welisagita@users.noreply.github.com>
2022-12-01 09:21:58 +0700

    Create 01.getting_started.js

---
+++ Js Introduction/01.getting_started.js
@@ -0,0 +1,18 @@
+//JavaScript is a popular programming language that has a wide range of applications.
+
+//JavaScript was previously used mainly for making webpages interactive such as form validation, animation, etc. 
+//Nowadays, JavaScript is also used in many other areas such as server-side development, mobile app development and so on.
+
+//Because of its wide range of applications, you can run JavaScript in several ways:
+
+//a. Using console tab of web browsers
+//b. Using Node.js
+//c. By creating web pages
+
+
+//printed the output
+console.log ("Hello World!") //Output Hello World!
+console.log ('Weli')
+
+
+//Use Javascript compiler to run the Code!
