2341c9ff0800c06d39684edbc9e1b072d6b516f0
Weli Sagita <91396511+Welisagita@users.noreply.github.com>
2022-12-01 16:15:24 +0700

    Create 03.javascript_for_loop.js

---
+++ Js Control Flow/03.javascript_for_loop.js
@@ -0,0 +1,21 @@
+//In programming, loops are used to repeat a block of code.
+
+//For example, if you want to show a message 100 times, then you can use a loop. 
+//It's just a simple example; you can achieve much more with loops.
+//This tutorial focuses on JavaScript for loop. You will learn about the other type of loops in the upcoming tutorials.
+
+
+//The syntax of the for loop is:
+
+//for (initialExpression; condition; updateExpression) {
+    // for loop body
+//}
+
+//Here,
+//1. The initialExpression initializes and/or declares variables and executes only once.
+//2. The condition is evaluated.
+//If the condition is false, the for loop is terminated.
+//If the condition is true, the block of code inside of the for loop body is executed.
+//3. The updateExpression updates the value of initialExpression when the condition is true.
+//4. The condition is evaluated again. This process continues until the condition is false.
+
