9ca50341cb0a4c498315a7a169e13360e7654caf
Weli Sagita <91396511+Welisagita@users.noreply.github.com>
2022-12-05 10:06:37 +0700

    Update 03.javascript_for_loop.js

--- Js Control Flow/03.javascript_for_loop.js
+++ Js Control Flow/03.javascript_for_loop.js
@@ -19,3 +19,39 @@
 //3. The updateExpression updates the value of initialExpression when the condition is true.
 //4. The condition is evaluated again. This process continues until the condition is false.
 
+
+//Example
+// i = 1; the variable is assign the value 1
+// i <= 5; now this variable is tasted, since this is true (1 is smaller then 5), the body will executed and printed the body and
+// give us the Output:
+/*Learning JavaScript
+JavaScript is Fun
+1*/
+// i++ ; and then the expression will executed which increases the value of i (by 1 to 2)
+//This process continues from 1 to 5 because the condition is i <=5
+//When the value becomes 6, this tes condition become false (i <= 5), that's why the loop process will be terminated
+for (let i = 1; i <= 5; i++) {
+     console.log("Learning JavaScript");
+     console.log("JavaScript is Fun");
+
+     console.log(i);
+}
+
+//Output:
+/*
+Learning JavaScript
+JavaScript is Fun
+1
+Learning JavaScript
+JavaScript is Fun
+2
+Learning JavaScript
+JavaScript is Fun
+3
+Learning JavaScript
+JavaScript is Fun
+4
+Learning JavaScript
+JavaScript is Fun
+5
+*/
