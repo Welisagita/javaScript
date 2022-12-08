fd298fec2b0150bfe0f9eb6896e58222ab973d13
Weli Sagita <91396511+Welisagita@users.noreply.github.com>
2022-12-08 15:31:58 +0000

    Update

--- Js Control Flow/04.javascript_while_loop.js
+++ Js Control Flow/04.javascript_while_loop.js
@@ -39,3 +39,22 @@ while (count < 5) {
     count = count + 1;
 }
 
+//If JavaScript is showing reference error “Prompt is not defined” you should do this: 
+//Solution 1: The most effective solution is we have to install “prompt-sync“. 
+//make sure you have also installed an updated version of npm and node, then write the below code in the terminal: npm install prompt-sync
+//Example: This example will demonstrate the use of prompt on the server side by using the “prompt-sync” package:
+//const prompt=require("prompt-sync")({sigint:true}); >> use this before the prompt
+
+const prompt=require("prompt-sync")({sigint:true});//This line i use because showing "ReferenceError: prompt is not defined"
+
+
+// ======================CREATING MULTIPLICATION TABLE=============================================
+
+const number = parseInt(prompt("Enter a number:"));
+
+let count1 = 1;
+
+while (count1 < 10) {
+    const product = number * count1;
+    console.log(product);
+}
