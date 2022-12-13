1908055594421e667a4f82ea7bfddb6e6870c18d
Weli Sagita <91396511+Welisagita@users.noreply.github.com>
2022-12-13 09:54:11 +0000

    update

---
+++ 003 Js Functions/01.javascript_function.js
@@ -0,0 +1,84 @@
+//JavaScript Function and Function Expressions
+/*
+In this tutorial, you will learn about 
+JavaScript function and function expressions 
+with the help of examples.
+
+JavaScript Function
+A function is a block of code that performs a specific task.
+
+Suppose you need to create a program to create a circle and color it. 
+You can create two functions to solve this problem:
+
+a function to draw the circle
+a function to color the circle
+Dividing a complex problem into smaller chunks 
+makes your program easy to understand and reusable.
+
+JavaScript also has a huge number of inbuilt functions. 
+For example, Math.sqrt() is a function to calculate the square root of a number.
+*/
+
+//The syntax to declare a function is:
+/*
+function nameOfFunction () {
+    // function body   
+}
+*/
+/*
+A function is declared using the function keyword.
+The basic rules of naming a function are similar to naming a variable. 
+It is better to write a descriptive name for your function. 
+For example, if a function is used to add two numbers, you could name the function add or addNumbers.
+The body of function is written within {}.
+*/
+
+//============Example==============
+// declaring a function named greet()
+function greet(){
+    console.log("Hello Everybody!");
+}
+greet() //function call
+
+/*Output
+Hello Everybody!
+ */
+
+//==============================Function Parameters==========================
+//A function can also be declared with parameters. 
+//A parameter is a value that is passed when declaring a function.
+
+// program to print the text
+// declaring a function
+function greet(name) {
+    console.log("Hello " + name + ":)");
+}
+
+// variable name can be different
+let name = prompt("Enter a name: ");
+
+// calling function
+greet(name);
+
+/*Output:
+Enter a name: weli
+Hello weli :)
+ */
+
+
+//===================Add two Numbers============
+
+// program to add two numbers using a function
+// declaring a function
+function add(a, b) {
+    console.log(a + b);
+}
+
+// calling functions
+add(3,4);
+add(2,9);
+
+/*Output
+7
+11
+ */
--- 003 Js Functions/main.js
+++ 003 Js Functions/main.js
@@ -6,49 +6,11 @@
 const prompt=require("prompt-sync")({sigint:true});//This line i use because showing "ReferenceError: prompt is not defined"
 
 
-//Take the number input
-const number5 = parseInt(prompt("Enter a number: "));
-
-switch (number5){
-    case 1:
-        console.log("January");
-        break;
-    case 2:
-        console.log("February");
-        break;
-    case 3:
-        console.log("March");
-        break;
-    case 4:
-        console.log("April");
-        break;
-    case 5:
-        console.log("May");
-        break;
-    case 6:
-        console.log("June");
-        break;
-    case 7:
-        console.log("July");
-        break;
-    case 8:
-        console.log("August");
-        break;
-    case 9:
-        console.log("September");
-        break;
-    case 10:
-        console.log("October");
-        break;
-    case 11:
-        console.log("November");
-        break;
-    case 12:
-        console.log("December");
-        break;
-    default:
-        console.log("Invalid number");
+function add (a, b){
+    console.log(a + b);
 }
+add(3, 9);
+add( 5, 1);
 
 
 
