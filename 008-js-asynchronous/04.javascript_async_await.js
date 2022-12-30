b39c38680591deb77791f23abd982af5d1ef8a32
Weli Sagita <welipalumbo@gmail.com>
2022-12-30 15:20:17 +0700

    update

--- 008-js-asynchronous/04.javascript_async_await.js
+++ 008-js-asynchronous/04.javascript_async_await.js
@@ -132,9 +132,9 @@ let promise4 = new Promise(function(resolve, reject){
     setTimeout(function(){
         resolve('Promise resolved')
     }, 4000);
-});
-
-async function asyncFunc(){
+  });
+  
+  async function asyncFunc(){
     try{
         let result4 = await promise4;
         console.log(result4)
@@ -142,6 +142,26 @@ async function asyncFunc(){
     catch (error){
         console.log(error);
     }
-}
+  }
+  
+  asyncFunc();//Promise resolved
 
-asynFunc();
+  /*In the above program, 
+  we have used try/catch block to handle the errors. 
+  If the program runs successfully, 
+  it will go to the try block. 
+  And if the program throws an error, it will go to the catch block. */
+  
+  
+  //======= Benefits of Using async Function
+  /*
+  1. The code is more readable than using a callback or a promise.
+  2. Error handling is simpler.
+  3. Debugging is easier.*/
+  
+  /*Note: These two keywords async/await 
+  were introduced in the newer version of JavaScript (ES8). 
+  Some older browsers may not support the use of async/await. 
+  To learn more, visit JavaScript async/await browser support. */
+  
+  
